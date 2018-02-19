c56a6fc70d97870de634fc82f0165830b072b6b4
Vladimir Ivanov <vladimir.vs.ivanov@gmail.com>
2018-02-19 11:07:34 +0300

    Add tests for coroutines implementation of ApiClient

--- app/src/test/java/com/epam/talks/github/model/ApiClientImplTest.kt
+++ app/src/test/java/com/epam/talks/github/model/ApiClientImplTest.kt
@@ -1,14 +1,48 @@
 package com.epam.talks.github.model
 
+import io.mockk.every
+import io.mockk.mockk
+import io.mockk.staticMockk
+import io.mockk.use
+import khttp.get
+import khttp.responses.GenericResponse
+import khttp.structures.authorization.BasicAuthorization
+import kotlinx.coroutines.experimental.runBlocking
+import org.json.JSONObject
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertNotNull
 import org.junit.Test
-
-import org.junit.Assert.*
 
 class ApiClientImplTest {
 
-	@Test
-	fun login() {
+	private val loginJson = "{ \"login\": \"login\", \"id\": 1, \"repos_url\": \"url\", \"name\": \"name\" }"
 
+@Test
+fun login() {
+	val apiClientImpl = ApiClient.ApiClientImpl()
+	val genericResponse = mockLoginResponse()
+
+	staticMockk("khttp.KHttp").use {
+		every { get("https://api.github.com/user", auth = any()) } returns genericResponse
+
+		runBlocking {
+			val githubUser =
+					apiClientImpl
+						.login(BasicAuthorization("login", "pass"))
+						.await()
+
+			assertNotNull(githubUser)
+			assertEquals("name", githubUser.name)
+			assertEquals("url", githubUser.repos_url)
+		}
+	}
+}
+
+	private fun mockLoginResponse(): GenericResponse {
+		val genericResponse = mockk<GenericResponse>()
+		every { genericResponse.jsonObject } returns JSONObject(loginJson)
+		every { genericResponse.statusCode } returns 200
+		return genericResponse
 	}
 
 	@Test
